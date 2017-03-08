478aa15ef8fe7636338be16f36098a7d6ad55982
Victor Perepelitsky <victor.prp@gmail.com>
2017-03-08 22:39:53 +0200

    logging is working (slf4j)

--- src/main/java/seemedics/triage/rest/TriageResource.java
+++ src/main/java/seemedics/triage/rest/TriageResource.java
@@ -1,5 +1,6 @@
 package seemedics.triage.rest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @author victorp
  */
 
-
+@Slf4j
 @RestController
 @RequestMapping("/v0/triage")
 public class TriageResource {
 
-
     @RequestMapping("")
     @ResponseBody()
     String root() {
+        log.debug("/v0/triage is called");
         return "This is the root triage end point";
     }
 
