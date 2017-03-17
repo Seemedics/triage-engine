878c7c5206348e5cc8e2fcdbedf040fd9fb92db7
Victor Perepelitsky <victor.prp@gmail.com>
2017-03-18 01:12:08 +0200

    triage engine - init test

--- src/main/java/seemedics/triage/engine/TriageProtocolSelector.java
+++ src/main/java/seemedics/triage/engine/TriageProtocolSelector.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 import java.util.Set;
 
 /**
+ * Selects corresponding protocol according to the given initial facts of a user.
+ *
  * @author victorp
  */
 public interface TriageProtocolSelector {
--- src/main/java/seemedics/triage/engine/TriageProtocols.java
+++ src/main/java/seemedics/triage/engine/TriageProtocols.java
@@ -6,12 +6,32 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
+ * This service provides a convenient way to access triage protocols
+ *
+ *
+ * 1. By contract this service must be very fast (Most operations will be executed without I/O)
+ * <p>
+ *
+ * 2. A given protocol is immutable forever. <p>
+ * Meaning: by given protocol id this service will always return exactly the same protocol. <p>
+ * It is critical property that ensures system consistency <p>
+ * since a user may be in the middle of any protocol and any change <p>
+ * in a protocol may lead to unpredictable behaviour. <p>
+ * In order to implement this idea an update to any protocol must be <p>
+ * implemented by creating new one that somehow will be signed by a newer version.
+ *
  * @author victorp
  */
 public interface TriageProtocols {
 
+    /**
+     * @return all existing protocols
+     */
     Stream<TriageProtocol> stream();
 
+    /**
+     * @return protocol by its ID
+     */
     Optional<TriageProtocol> get(String id);
 
 
