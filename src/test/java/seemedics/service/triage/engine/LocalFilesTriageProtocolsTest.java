a4d28f360cadba78b32cef43c943eda2737844a2
constructor-igor <igor.zc@gmail.com>
2017-04-05 17:06:38 +0300

    MVP-23: created test class LocalFilesTriageProtocolsTest

---
+++ src/test/java/seemedics/service/triage/engine/LocalFilesTriageProtocolsTest.java
@@ -0,0 +1,17 @@
+package seemedics.service.triage.engine;
+
+import org.junit.Test;
+
+/**
+ * Created by igor-z on 05-Apr-17.
+ */
+public class LocalFilesTriageProtocolsTest {
+    @Test
+    public void stream() throws Exception {
+    }
+
+    @Test
+    public void get() throws Exception {
+    }
+
+}
