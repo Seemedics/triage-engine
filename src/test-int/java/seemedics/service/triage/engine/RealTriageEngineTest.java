0352751c4af7c8644eec5579b665bab7d7f5e039
constructor-igor <igor.zc@gmail.com>
2017-05-08 13:39:07 +0300

    MVP-18: completed simple protocol of RealTriageEngineTest

--- src/test-int/java/seemedics/service/triage/engine/RealTriageEngineTest.java
+++ src/test-int/java/seemedics/service/triage/engine/RealTriageEngineTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import seemedics.model.Fact;
+import seemedics.model.dialog.PredefAnswer;
+import seemedics.model.dialog.Question;
 
 import java.util.*;
 
@@ -29,8 +31,32 @@ public class RealTriageEngineTest {
         values.put("temperature", "37-39");
         Fact initialFact = new Fact("fact-fever-37-39", "Body Temperature 37-39", "sym-sore-throat", "EXISTS", timePeriodMills, values);
         TriageStartIn triageStartIn = new TriageStartIn(new HashSet(Arrays.asList(initialFact)));
-        TriageResult triageResult = triageEngine.start(triageStartIn);
 
+        TriageResult triageResult = triageEngine.start(triageStartIn);
         Assert.assertFalse("Result #1 must not be final", triageResult.isFinal());
+
+        int questionCount = 1;
+
+        HashSet<Fact> allFacts = new HashSet<>();
+        while (!triageResult.isFinal()){
+            allFacts.addAll(triageResult.getNewFacts());
+
+            Question question = triageResult.getQuestion().get();
+            log.info("Question #{} {}",questionCount, question);
+
+            PredefAnswer userAnswer = question.getChoices().stream().findFirst().get();
+            log.info("User Answer #{} {}", questionCount, userAnswer);
+
+            TriageNextIn triageNextIn = new TriageNextIn(triageResult.getProtocolId(), triageResult.getStepId(), userAnswer.getId(), allFacts);
+            triageResult = triageEngine.next(triageNextIn);
+
+            questionCount++;
+        }
+
+        //Final Result
+        allFacts.addAll(triageResult.getNewFacts());
+
+        log.info("Triage urgency: {}", triageResult.getUrgency().get());
+        log.info("All facts from triage: {}", allFacts);
     }
 }
